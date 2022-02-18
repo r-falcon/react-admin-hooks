e412a0e6024c8a68d8fa170bc49ea9d95a430135
r-falcon <rfalcon@163.com>
2022-02-18 14:46:53 +0800

    fix: perfect login model

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { isLogin } from '@/utils/auth'
+import { Redirect } from 'react-router'
 
 function BasicLayout() {
-  return <h2>整体布局</h2>
+  if (isLogin()) {
+    return <h2>logined out</h2>
+  } else {
+    return <Redirect to="/login" />
+  }
 }
 
 export default BasicLayout
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,78 @@
 import React from 'react'
+import styles from './index.less'
+import { Form, Input, Button, Checkbox, message } from 'antd'
+import { getLocalUser, setLocalUser, setToken, setUserInfo } from '@/utils/auth'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { loginApi } from '@/services/login'
+import router from 'umi/router'
 
 function Login() {
-  return <h2>登录页</h2>
+  const IPT_RULE_USERNAME = [
+    {
+      required: true,
+      message: '请输入用户名:admin'
+    }
+  ]
+
+  const IPT_RULE_PASSWORD = [
+    {
+      required: true,
+      message: '请输入密码:123456'
+    }
+  ]
+
+  const onFinish = values => {
+    const { username, password, remember } = values
+    setLocalUser({ username: username, password: password, remember: remember })
+    loginApi({ username: username, password: password })
+      .then(res => {
+        console.log('请求成功', res)
+        setToken(res.data.token)
+        setUserInfo(res.data)
+        message.success(res.meta.msg)
+        router.push('/')
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <div className={styles.loginBox}>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>umi-dva-antd</div>
+        <div className={styles.welcome}>欢迎使用，请先登录</div>
+
+        <Form
+          className={styles.formBox}
+          initialValues={
+            getLocalUser() && getLocalUser().remember === true
+              ? getLocalUser()
+              : { remember: false }
+          }
+          onFinish={onFinish}
+        >
+          <Form.Item name="username" rules={IPT_RULE_USERNAME}>
+            <Input prefix={<UserOutlined />} placeholder="账号：admin" />
+          </Form.Item>
+
+          <Form.Item name="password" rules={IPT_RULE_PASSWORD}>
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="密码：123456"
+            />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+
+          <Form.Item style={{ marginTop: '10px' }}>
+            <Button type="primary" htmlType="submit" className={styles.btn}>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 export default Login
